f2cdae95b507141726664ecde8de307a36126257
namoo36 <wldnrkwhr36@naver.com>
2025-04-22 14:09:06 +0900

    20250422

---
+++ bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/BonbonApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.Team3.bonbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonbonApplication.class, args);
+	}
+
+}

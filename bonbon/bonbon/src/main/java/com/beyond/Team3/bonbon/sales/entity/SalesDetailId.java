3dd24e0b263890a21237fa423e97cd1e925f1d9b
kimdoyun0806 <gim_doyun@naver.com>
2025-05-07 09:31:10 +0900

    [feat] 매출 분석 기능 구현 (김도윤)

--- bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/sales/entity/SalesDetailId.java
+++ bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/sales/entity/SalesDetailId.java
@@ -1,15 +1,16 @@
 package com.beyond.Team3.bonbon.sales.entity;
 
-
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Embeddable
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode

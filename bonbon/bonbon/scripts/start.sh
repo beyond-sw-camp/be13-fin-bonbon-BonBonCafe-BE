fd142aeb34e780174c845c679931f6ceb378c301
namoo36 <wldnrkwhr36@naver.com>
2025-05-12 12:28:55 +0900

    [CI/CD] Update start.sh(이승용)
    [CI/CD] Update start.sh(이승용)

--- bonbon/bonbon/scripts/start.sh
+++ bonbon/bonbon/scripts/start.sh
@@ -2,6 +2,7 @@
 
 # 프로젝트 루트 디렉토리 설정
 PROJECT_ROOT="/home/ec2-user/app/build/libs"
+ZIP_FILE="deployment.zip"
 JAR_NAME="bonbon-0.0.1-SNAPSHOT.jar"
 JAR_FILE="$PROJECT_ROOT/$JAR_NAME"
 
@@ -13,14 +14,23 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 # 현재 시간 저장
 TIME_NOW=$(date '+%Y-%m-%d %H:%M:%S')
 
-# JAR 파일 다운로드 (S3에서)
-echo "$TIME_NOW > S3에서 JAR 파일 다운로드 시작" >> $DEPLOY_LOG
-aws s3 cp s3://bonbon-back-end-bucket/bonbon.jar $JAR_FILE>> $DEPLOY_LOG 2>&1
+# S3에서 ZIP 파일 다운로드
+echo "$TIME_NOW > S3에서 ZIP 파일 다운로드 시작" >> $DEPLOY_LOG
+aws s3 cp s3://bonbon-back-end-bucket/$ZIP_FILE $PROJECT_ROOT/$ZIP_FILE >> $DEPLOY_LOG 2>&1
 if [ $? -ne 0 ]; then
-  echo "$TIME_NOW > 오류: S3에서 JAR 파일 다운로드 실패" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 오류: S3에서 ZIP 파일 다운로드 실패" >> $DEPLOY_LOG
   exit 1
 fi
-echo "$TIME_NOW > JAR 파일 다운로드 완료" >> $DEPLOY_LOG
+echo "$TIME_NOW > ZIP 파일 다운로드 완료" >> $DEPLOY_LOG
+
+# ZIP 파일 압축 해제
+echo "$TIME_NOW > ZIP 파일 압축 해제 시작" >> $DEPLOY_LOG
+unzip $PROJECT_ROOT/$ZIP_FILE -d $PROJECT_ROOT >> $DEPLOY_LOG 2>&1
+if [ $? -ne 0 ]; then
+  echo "$TIME_NOW > 오류: ZIP 파일 압축 해제 실패" >> $DEPLOY_LOG
+  exit 1
+fi
+echo "$TIME_NOW > ZIP 파일 압축 해제 완료" >> $DEPLOY_LOG
 
 # 기존 프로세스 종료 (있을 경우)
 EXISTING_PID=$(pgrep -f "$JAR_FILE")
@@ -29,7 +39,7 @@ if [ -n "$EXISTING_PID" ]; then
   kill -9 $EXISTING_PID
 fi
 
-# jar 파일 실행
+# JAR 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행 시작" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 

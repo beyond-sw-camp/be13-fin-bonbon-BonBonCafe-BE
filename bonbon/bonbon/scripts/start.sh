d309d44f9c3f679c66bf43c85dc7e7f6c659da51
namoo36 <wldnrkwhr36@naver.com>
2025-05-12 12:36:04 +0900

    [CI/CD] Update start.sh (이승용)
    그만할래..

--- bonbon/bonbon/scripts/start.sh
+++ bonbon/bonbon/scripts/start.sh
@@ -2,7 +2,6 @@
 
 # 프로젝트 루트 디렉토리 설정
 PROJECT_ROOT="/home/ec2-user/app/build/libs"
-ZIP_FILE="deployment.zip"
 JAR_NAME="bonbon-0.0.1-SNAPSHOT.jar"
 JAR_FILE="$PROJECT_ROOT/$JAR_NAME"
 
@@ -14,9 +13,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 # 현재 시간 저장
 TIME_NOW=$(date '+%Y-%m-%d %H:%M:%S')
 
+# GitHub SHA 값 가져오기 (GitHub Actions에서 사용되는 값)
+GITHUB_SHA=$1 
+
 # S3에서 ZIP 파일 다운로드
 echo "$TIME_NOW > S3에서 ZIP 파일 다운로드 시작" >> $DEPLOY_LOG
-aws s3 cp s3://bonbon-back-end-bucket/$ZIP_FILE $PROJECT_ROOT/$ZIP_FILE >> $DEPLOY_LOG 2>&1
+aws s3 cp s3://bonbon-back-end-bucket/$GITHUB_SHA.zip $PROJECT_ROOT/$GITHUB_SHA.zip >> $DEPLOY_LOG 2>&1
 if [ $? -ne 0 ]; then
   echo "$TIME_NOW > 오류: S3에서 ZIP 파일 다운로드 실패" >> $DEPLOY_LOG
   exit 1

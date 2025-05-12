7f512c021c08cace3f9552fbae0903c862096d3d
namoo36 <wldnrkwhr36@naver.com>
2025-05-12 11:50:49 +0900

    [ci/cd] CI/CD - start.sh 수정 (이승용)
    [ci/cd] CI/CD - start.sh 수정 (이승용)

--- bonbon/bonbon/scripts/start.sh
+++ bonbon/bonbon/scripts/start.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-chmod +x /home/ec2-user/app/scripts/start.sh
-
 # 프로젝트 루트 디렉토리 설정
 PROJECT_ROOT="/home/ec2-user/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+JAR_NAME="bonbon-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/$JAR_NAME"
 
 # 로그 파일 경로
 APP_LOG="$PROJECT_ROOT/application.log"
@@ -16,7 +15,7 @@ TIME_NOW=$(date '+%Y-%m-%d %H:%M:%S')
 
 # JAR 파일 다운로드 (S3에서)
 echo "$TIME_NOW > S3에서 JAR 파일 다운로드 시작" >> $DEPLOY_LOG
-aws s3 cp s3://bonbon-back-end-bucket/spring-webapp.jar $JAR_FILE >> $DEPLOY_LOG 2>&1
+aws s3 cp s3://bonbon-back-end-bucket/build/libs/$JAR_NAME $JAR_FILE>> $DEPLOY_LOG 2>&1
 if [ $? -ne 0 ]; then
   echo "$TIME_NOW > 오류: S3에서 JAR 파일 다운로드 실패" >> $DEPLOY_LOG
   exit 1
@@ -35,4 +34,9 @@ echo "$TIME_NOW > $JAR_FILE 파일 실행 시작" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
+if [ -z "$CURRENT_PID" ]; then
+  echo "$TIME_NOW > 애플리케이션 실행 실패" >> $DEPLOY_LOG
+  exit 1
+fi
+
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG

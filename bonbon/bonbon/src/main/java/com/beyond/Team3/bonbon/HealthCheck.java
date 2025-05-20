29149819b3a7319054061eb02bec317e25c68bc6
namoo36 <wldnrkwhr36@naver.com>
2025-05-20 14:27:27 +0900

    [CI/CD] HealthCheck 추가 (이승용)

---
+++ bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/HealthCheck.java
@@ -0,0 +1,14 @@
+package com.beyond.Team3.bonbon;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}

888ec894a56eba32200b149b5037212204ed48ce
mlnstone <dnswjs1776@naver.com>
2025-05-26 11:40:57 +0900

    [fix] 주문 동시성(김민석)

--- bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/headquaterStock/repository/HeadquarterStockRepository.java
+++ bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/headquaterStock/repository/HeadquarterStockRepository.java
@@ -3,9 +3,13 @@ package com.beyond.Team3.bonbon.headquaterStock.repository;
 import com.beyond.Team3.bonbon.headquarter.entity.Headquarter;
 import com.beyond.Team3.bonbon.headquaterStock.entity.HeadquarterStock;
 import com.beyond.Team3.bonbon.ingredient.entity.Ingredient;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +18,15 @@ public interface HeadquarterStockRepository extends JpaRepository<HeadquarterSto
     Page<HeadquarterStock> getAllStock(Pageable pageable, Long headquarterId, String search);
 
     boolean existsByHeadquarterAndIngredient(Headquarter headquarter, Ingredient ingredient);
-
-
+    
     Optional<HeadquarterStock> findByHeadquarter_HeadquarterIdAndIngredient_IngredientId(Long headquarterId, Long ingredientId);
 
     List<HeadquarterStock> findByHeadquarterIdWithIngredient(Long headquarterId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM HeadquarterStock s WHERE s.headquarter.headquarterId = :hqId AND s.ingredient.ingredientId = :ingredientId")
+    Optional<HeadquarterStock> findByHeadquarterAndIngredientWithLock(
+            @Param("hqId") Long headquarterId,
+            @Param("ingredientId") Long ingredientId
+    );
 }

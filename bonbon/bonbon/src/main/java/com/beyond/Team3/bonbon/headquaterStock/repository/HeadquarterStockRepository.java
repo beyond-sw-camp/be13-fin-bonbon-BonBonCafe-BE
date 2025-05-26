9a859946c1bbe2d3bcc0fd86804095daedc8ffae
mlnstone <dnswjs1776@naver.com>
2025-05-26 14:10:00 +0900

    [fix] 주문 동시성(김민석)

--- bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/headquaterStock/repository/HeadquarterStockRepository.java
+++ bonbon/bonbon/src/main/java/com/beyond/Team3/bonbon/headquaterStock/repository/HeadquarterStockRepository.java
@@ -3,22 +3,24 @@ package com.beyond.Team3.bonbon.headquaterStock.repository;
 import com.beyond.Team3.bonbon.headquarter.entity.Headquarter;
 import com.beyond.Team3.bonbon.headquaterStock.entity.HeadquarterStock;
 import com.beyond.Team3.bonbon.ingredient.entity.Ingredient;
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface HeadquarterStockRepository extends JpaRepository<HeadquarterStock, Long>, HeadquarterStockRepositoryCustom {
     Page<HeadquarterStock> getAllStock(Pageable pageable, Long headquarterId, String search);
 
     boolean existsByHeadquarterAndIngredient(Headquarter headquarter, Ingredient ingredient);
-    
+
     Optional<HeadquarterStock> findByHeadquarter_HeadquarterIdAndIngredient_IngredientId(Long headquarterId, Long ingredientId);
 
     List<HeadquarterStock> findByHeadquarterIdWithIngredient(Long headquarterId);
